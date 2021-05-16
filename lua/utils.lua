bb541348da2e14bfc237a1146e2eb0e816fa4e22
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-05-16 20:25:53 +0530

    utils functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,6 +1,6 @@
 local utils = {}
 
-keymap = function(mode, lhs, rhs, opts)
+utils.keymap = function(mode, lhs, rhs, opts)
     local options = {noremap = true}
     if opts then options = vim.tbl_extend("force", options, opts) end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
