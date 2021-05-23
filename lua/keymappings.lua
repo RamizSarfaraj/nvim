a07a92392045a9a97e3eaee4131afca03b4e226e
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-05-23 21:15:32 +0530

    few new keybindings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -18,12 +18,26 @@ map("n", "<C-K>", "<C-W>k")
 map("n", "<C-H>", "<C-W>h")
 map("n", "<C-L>", "<C-W>l")
 
+--yank util the end of line
+map('n', 'Y', 'y$', { noremap = true })
 -- split
-map("n", "<M-v>", ":vsp<CR>")
-map("n", "<M-h>", ":sp<CR>")
+map("n", "|", ":vsp<CR>")
+map("n", "-", ":sp<CR>")
 
 -- resize split window
 map("n", "<C-right>", ":vertical resize +2<CR>")
 map("n", "<C-left>", ":vertical resize -2<CR>")
 map("n", "<C-up>", ":resize +2<CR>")
 map("n", "<C-down>", ":resize -2<CR>")
+
+-- Move selected line / block of text in visual mode
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
+
+-- Tab switch buffer
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
+
+-- better indenting
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
