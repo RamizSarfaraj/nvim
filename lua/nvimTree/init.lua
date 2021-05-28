41fb7564209e87feac5e1e7e870a22aaea728cd9
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-05-28 19:26:55 +0530

    made toggling tree silent.

--- lua/nvimTree/init.lua
+++ lua/nvimTree/init.lua
@@ -4,10 +4,10 @@ local g = vim.g
 g.nvim_tree_side = "left"
 g.nvim_tree_width = 30
 g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
-g.nvim_tree_auto_open = 0
+g.nvim_tree_auto_open = 1
 g.nvim_tree_auto_close = 1
 g.nvim_tree_quit_on_open = 1
-g.nvim_tree_follow = 1
+g.nvim_tree_follow = 0
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_git_hl = 1
@@ -45,7 +45,7 @@ vim.api.nvim_set_keymap(
 	":NvimTreeToggle<CR>",
 	{
 		noremap = true,
-		silent = false
+		silent = true
 	}
 )
 
