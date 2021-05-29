860a4b59d56444d13c0dc62e43ca5e972f8fea2f
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-05-29 11:41:35 +0530

    new nvcode color

--- lua/general/colorcheme.lua
+++ lua/general/colorcheme.lua
@@ -2,15 +2,15 @@
 vim.o.termguicolors = true -- set term gui colors most terminals support this
 vim.o.background = 'dark'
 
- --gruvbox colorscheme
--- vim.g.gruvbox_bold = 1
--- vim.g.gruvbox_italic = 1
--- vim.g.gruvbox_contrast_dark = 'hard'
--- vim.g.gruvbox_transparent_bg = 1
--- vim.g.gruvbox_hls_cursor = 'orange'
--- vim.g.gruvbox_color_column = "green"
--- vim.g.gruvbox_italicize_strings = 1
--- vim.cmd("colorscheme gruvbox")
+--gruvbox colorscheme
+vim.g.gruvbox_bold = 1
+vim.g.gruvbox_italic = 1
+vim.g.gruvbox_contrast_dark = 'hard'
+vim.g.gruvbox_transparent_bg = 1
+vim.g.gruvbox_hls_cursor = 'orange'
+vim.g.gruvbox_color_column = "green"
+vim.g.gruvbox_italicize_strings = 1
+vim.cmd("colorscheme gruvbox")
 
 -- material colorscheme
 -- vim.g.material_style = 'darker'
@@ -22,18 +22,3 @@ vim.o.background = 'dark'
 -- vim.g.material_borders = false
 -- vim.g.material_disable_background = false
 -- vim.cmd("colorscheme material")
-
-
--- tokyonight
--- vim.g.tokyonight_style = "night"
--- vim.g.tokyonight_italic_functions = true
--- vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-
--- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-
--- vim.cmd[[colorscheme tokyonight]]
-
-
--- embark
-vim.g.embark_terminal_italics = 1
-vim.cmd[[colo embark]]
