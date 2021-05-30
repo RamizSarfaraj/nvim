387b16c67e0496fc85003d5b3d50b12c0ab8c993
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-05-30 07:45:42 +0530

    telescope

--- lua/telescope-nvim/init.lua
+++ lua/telescope-nvim/init.lua
@@ -11,7 +11,7 @@ require('telescope').setup{
       '--column',
       '--smart-case'
     },
-    prompt_position = "bottom",
+    prompt_position = "top",
     prompt_prefix = "> ",
     selection_caret = "> ",
     entry_prefix = "  ",
