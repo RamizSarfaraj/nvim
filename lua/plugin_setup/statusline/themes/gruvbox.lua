26f6021b18146c0380e7e95c77380a5d78cf33ca
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-19 20:24:33 +0530

    added option for gruvbox color in galaxy line

---
+++ lua/plugin_setup/statusline/themes/gruvbox.lua
@@ -0,0 +1,12 @@
+local color = {
+  bg = "#282828",
+  fg = "#bdae93",
+  end_color = "#689d6a",
+  mode_color = "#fe8019",
+  lightbg = "#504945",
+  left_most = "#b8bb26",
+  error_color = "#cc241d",
+  git_color = "#b16286"
+}
+
+return color
