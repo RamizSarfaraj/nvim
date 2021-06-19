b1f6478b73b627306b1b3b31631d1e16427d754e
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-19 21:24:21 +0530

    tokyonight colors

---
+++ lua/plugin_setup/statusline/themes/tokyonight.lua
@@ -0,0 +1,12 @@
+local color = {
+  bg = "#1f2335",
+  fg = "#c0caf5",
+  end_color = "#9ece6a",
+  mode_color = "#bb9af7",
+  lightbg = "#24283b",
+  left_most = "#ff9e64",
+  error_color = "#f7768e",
+  git_color = "#0db947"
+}
+
+return color
