d8de8a9e420454faa77bba2aa369cfa9f5ddb819
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-19 20:42:53 +0530

    dracula colors for galaxy line

---
+++ lua/plugin_setup/statusline/themes/dracula.lua
@@ -0,0 +1,13 @@
+local color = {
+  bg = "#282A36",
+  fg = "#F8F8F2",
+  end_color = "#50FA7B",
+  mode_color = "#FF79C6",
+  lightbg = "#504945",
+  left_most = "#8BE9FD",
+  error_color = "#cc241d",
+  git_color = "#6272A4",
+}
+
+return color
+
