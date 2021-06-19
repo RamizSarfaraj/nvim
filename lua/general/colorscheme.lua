cae6597ebb8148aca323c06757ff37d2ab8ae8ba
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-19 21:33:26 +0530

    tokyonight night version

--- lua/general/colorscheme.lua
+++ lua/general/colorscheme.lua
@@ -2,9 +2,9 @@ vim.o.termguicolors = true -- set term gui colors most terminals support this
 vim.o.background = 'dark'
 
 
-vim.cmd[[ colo tokyonight ]]
 
-vim.g.tokyonight_style = "storm"
+-- vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keywords = true
 vim.g.tokyonight_italic_functions = true
@@ -12,3 +12,4 @@ vim.g.tokyonight_italic_variables = true
 vim.g.tokyonight_dark_sidebar = true
 vim.g.tokyonight_dark_float = true
 
+vim.cmd[[ colo tokyonight ]]
