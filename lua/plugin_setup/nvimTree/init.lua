a81f018a6aa16e2dce3f0a417f0dbc65f03604a2
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-23 12:14:04 +0530

    now show hidden file

--- lua/plugin_setup/nvimTree/init.lua
+++ lua/plugin_setup/nvimTree/init.lua
@@ -9,9 +9,9 @@ g.nvim_tree_auto_close = 1
 g.nvim_tree_quit_on_open = 1
 g.nvim_tree_follow = 0
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 0
+g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
-g.nvim_tree_root_folder_modifier = ":~"
+g.nvim_tree_root_folder_modifier = ":t"
 g.nvim_tree_tab_open = 0
 g.nvim_tree_allow_resize = 1
 
