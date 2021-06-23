1947b1d89ab63e979e4a22254d40ae3be47abc63
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-23 11:32:12 +0530

    statusline colo

--- lua/plugin_setup/statusline/themes/dracula.lua
+++ lua/plugin_setup/statusline/themes/dracula.lua
@@ -3,8 +3,8 @@ local color = {
   fg = "#F8F8F2",
   end_color = "#50FA7B",
   mode_color = "#FF79C6",
-  lightbg = "#504945",
-  left_most = "#d6acff",
+  lightbg =  "#282A36",--"#504945",
+  left_most = "#ffb86c",
   error_color = "#cc241d",
   git_color = "#d6acff",
 }
