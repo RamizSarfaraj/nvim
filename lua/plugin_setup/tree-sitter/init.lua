c554d33aab2a5c162e26b3d980b1eb9b11e48611
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-24 23:07:15 +0530

    treesitter for javascript, html and css

--- lua/plugin_setup/tree-sitter/init.lua
+++ lua/plugin_setup/tree-sitter/init.lua
@@ -1,7 +1,10 @@
 
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = { 'bash', 'cpp', 'lua', 'python' },
+    ensure_installed = { 'bash', 'cpp', 'lua', 'python', 'javascript', 'html', 'css' },
     ignore_install = {},
+    indent = {
+      enable = true,
+    },
     highlight = {
         enable = true,
         disable = { },
