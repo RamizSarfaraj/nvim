a2acef46291a1f27585b467b72f8a0372df62107
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-27 22:02:17 +0530

    using ctrl-/ for comment

--- lua/plugin_setup/commenter/init.lua
+++ lua/plugin_setup/commenter/init.lua
@@ -8,7 +8,7 @@ require('nvim_comment').setup({
     -- Should key mappings be created
     create_mappings = true,
     -- Normal mode mapping left hand side
-    line_mapping = "<leader>//",
+    line_mapping = "<C-/>",
     -- Visual/Operator mapping left hand side
-    operator_mapping = "<leader>/"
+    operator_mapping = "<C-/>"
 })
