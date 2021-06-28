555fad02ade0d2d3940ec345d05c4862d3e809a2
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-06-29 00:56:32 +0530

    all the colors at same place

--- lua/general/colorscheme.lua
+++ lua/general/colorscheme.lua
@@ -2,36 +2,4 @@ local cmd = vim.cmd
 vim.o.termguicolors = true -- set term gui colors most terminals support this
 vim.o.background = 'dark'
 
-
-
---[[
--- vim.g.tokyonight_style = "storm"
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_comments = true
-vim.g.tokyonight_italic_keywords = true
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_variables = true
-vim.g.tokyonight_dark_sidebar = true
-vim.g.tokyonight_dark_float = true
-
-cmd[[ colo tokyonight ]]
---]]
-
-
---[[
-require("onedark").setup({
-  commentStyle = "italic",
-  keywordStyle = "italic",
-  functionStyle = "italic",
-  variableStyle = "italic",
-  transparent = true,
-  -- sidebars = {"packer"},
-  darkSidebar = true,
-  darkFloat = true,
-  colors = {hint = "orange", error = "#ff0000"}
-})
---]]
-
-
-
-cmd[[ colo dracula ]]
+cmd[[ colo base16-dracula ]]
