b54b479c2be21c56f746f2b0168235e7e6339055
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-07-02 12:48:42 +0530

    nvimTree new keybindings. and few changes

--- lua/plugin_setup/nvimTree/init.lua
+++ lua/plugin_setup/nvimTree/init.lua
@@ -1,27 +1,27 @@
 local cmd = vim.cmd
-local g = vim.g
+local G = vim.g
 
-g.nvim_tree_side = "right"
-g.nvim_tree_width = 35
-g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 1
-g.nvim_tree_quit_on_open = 1
-g.nvim_tree_follow = 0
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_root_folder_modifier = ":t"
-g.nvim_tree_tab_open = 0
-g.nvim_tree_allow_resize = 1
+G.nvim_tree_side = "left"
+G.nvim_tree_width = 32
+G.nvim_tree_ignore = {".git", "node_modules", ".cache"}
+G.nvim_tree_auto_open = 1
+G.nvim_tree_auto_close = 1
+G.nvim_tree_quit_on_open = 1
+G.nvim_tree_follow = 0
+G.nvim_tree_indent_markers = 1
+G.nvim_tree_hide_dotfiles = 1
+G.nvim_tree_git_hl = 1
+G.nvim_tree_root_folder_modifier = ":t"
+G.nvim_tree_tab_open = 0
+G.nvim_tree_allow_resize = 1
 
-g.nvim_tree_show_icons = {
+G.nvim_tree_show_icons = {
 	git = 1,
 	folders = 1,
 	files = 1
 }
 
-g.nvim_tree_icons = {
+G.nvim_tree_icons = {
 	default = " ",
 	symlink = " ",
 	git = {
@@ -38,7 +38,7 @@ g.nvim_tree_icons = {
 
 vim.api.nvim_set_keymap(
 	"n",
-	"<C-n>",
+	"<leader>e", -- e for explorer
 	":NvimTreeToggle<CR>",
 	{
 		noremap = true,
@@ -48,7 +48,7 @@ vim.api.nvim_set_keymap(
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 -- default mappings
-vim.g.nvim_tree_bindings = {
+G.nvim_tree_bindings = {
   { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
   { key = {"<2-RightMouse>", "<C-}>"},    cb = tree_cb("cd") },
   { key = "<C-v>",                        cb = tree_cb("vsplit") },
