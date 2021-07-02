6984d380ada6e3631dc8135a375eb86e730aedbd
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-07-02 23:09:36 +0530

    snazzy colorscheme

--- lua/general/colorscheme.lua
+++ lua/general/colorscheme.lua
@@ -2,4 +2,5 @@ local cmd = vim.cmd
 vim.o.termguicolors = true -- set term gui colors most terminals support this
 vim.o.background = 'dark'
 
-cmd[[ colo dracula ]]
+-- cmd[[ colo dracula ]]
+cmd[[ colo snazzy ]]
---
+++ lua/plugin_setup/statusline/themes/snazzy.lua
@@ -0,0 +1,20 @@
+local colors = {
+    bg = "#1e2127",
+    yellow = '#f3f99d',
+    dark_yellow = '#FABD2F',
+    cyan = '#9aedf3',
+    green = '#5af78e',
+    light_green = '#B8BB26',
+    string_orange = '#D65D0E',
+    orange = '#FE8019',
+    purple = '#ff6ac1',
+    magenta = '#D3869B',
+    grey = '#A89984',
+    blue = '#458588',
+    vivid_blue = '#4FC1FF',
+    light_blue = '#57c7ff',
+    red = '#FB4834',
+    error_red = '#CC241D',
+    info_yellow = '#D79921'
+}
+return colors
