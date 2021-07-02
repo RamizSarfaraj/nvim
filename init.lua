27174ae9c15111abbe2ec7681a7ca63b4b7ac330
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-07-02 23:10:16 +0530

    transparent disabled

--- init.lua
+++ init.lua
@@ -10,5 +10,5 @@ require[[general]]
 --
 require[[plugin_setup]]
 
-vim.cmd[[ hi Normal guibg=NONE ctermbg=NONE ]] -- make colorscheme transparent
+-- vim.cmd[[ hi Normal guibg=NONE ctermbg=NONE ]] -- make colorscheme transparent
 -- vim.cmd[[ hi Normal ctermbg=NONE ]] -- while using gui client if at all.
