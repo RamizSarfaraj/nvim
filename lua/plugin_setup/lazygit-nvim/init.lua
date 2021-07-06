47c2913c5b12c83f69247b19515957b1f8021c82
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-07-06 08:31:52 +0530

    lazygit in neovim

--- lua/plugin_setup/lazygit-nvim/init.lua
+++ lua/plugin_setup/lazygit-nvim/init.lua
@@ -1,7 +1,11 @@
 local G = vim.g
+local U = require[[utils]]
+local map = U.keymap
 
 G.lazygit_floating_window_winblend = 1 -- transparency of floating window
 G.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
 G.lazygit_floating_window_corner_chars = {'╭', '╮', '╰', '╯'} -- customize lazygit popup window corner characters
 G.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
 G.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+
+map('n', '<leader>lg', [[<cmd>LazyGit<CR>]])
