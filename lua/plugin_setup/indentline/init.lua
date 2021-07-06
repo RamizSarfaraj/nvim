4e6061d7569c82f19f9b37b6b9d4fbb58a2bae76
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-07-06 09:29:05 +0530

    little change

--- lua/plugin_setup/indentline/init.lua
+++ lua/plugin_setup/indentline/init.lua
@@ -7,4 +7,4 @@ g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard", "startif
 g.indent_blankline_buftype_exclude = {"terminal"}
 
 g.indent_blankline_show_trailing_blankline_indent = false
-g.indent_blankline_show_first_indent_level = true
+g.indent_blankline_show_first_indent_level = false
