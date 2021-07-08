4b00c49fc580999d23d3000bb6109cd61e11a712
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-07-08 11:12:41 +0530

    colors

--- lua/plugin_setup/statusline/init.lua
+++ lua/plugin_setup/statusline/init.lua
@@ -1,26 +1,4 @@
 local gl = require('galaxyline')
---[[
-local colors = {
-    -- bg = '#2E2E2E',
-    bg = '#292D38',
-    yellow = '#DCDCAA',
-    dark_yellow = '#D7BA7D',
-    cyan = '#4EC9B0',
-    green = '#608B4E',
-    light_green = '#B5CEA8',
-    string_orange = '#CE9178',
-    orange = '#FF8800',
-    purple = '#C586C0',
-    magenta = '#D16D9E',
-    grey = '#858585',
-    blue = '#569CD6',
-    vivid_blue = '#4FC1FF',
-    light_blue = '#9CDCFE',
-    red = '#D16969',
-    error_red = '#F44747',
-    info_yellow = '#FFCC66'
-}
---]]
 
 local colors = require[[plugin_setup.statusline.themes.snazzy]]
 
